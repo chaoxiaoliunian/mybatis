3afda16ddc7ebbd736b288657845d14a40bdfae0
sjqi <1013988871@qq.com>
2019-03-24 09:41:40 +0800

    新建mybatis 建造模块

---
+++ build-mybatis/src/test/java/TestFind.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+/**
+ * @ClassName TestFindOne
+ * @Description 测试查找功能类
+ * @Author sjqi
+ * @Date 19:38 2019/3/23
+ * @Version 1.0
+ **/
+public class TestFind {
+
+    public void init() {
+        //TODO:加载配置文件
+    }
+
+    @Test
+    public void testFindOne() {
+        init();
+        //执行查找
+    }
+}
